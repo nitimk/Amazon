b0f8a396d810666f46374f9864e219ebb704e21b
nitimk <nitimk@gmail.com>
2022-05-26 17:09:29 -0400

    update

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -14,7 +14,7 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 
 import { Store } from '../Store';
-import { getError } from '../../utils';
+import { getError } from '../utils';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
